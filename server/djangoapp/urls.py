ba6eed6b8f39986f2e6a514fdf27714190d10c62
Andile <andilejonas71@gmail.com>
2024-06-29 03:19:59 -0400

    Added CarMake and CarModel models, populate script, and updated views

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,7 +14,12 @@ path(route='register', view=views.registration, name='register'),
   # path for logout
     path(route='logout', view=views.logout_request, name='logout'),
 
+# path for logout
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
+
     # path for dealer reviews view
+
+
 
     # path for add a review view
 
